aa933d8f728b2e4b1489dc9e559dacbd59450e62
kekhuay <kekhuay@users.noreply.github.com>
2020-12-11 15:54:52 +0000

    Update from https://github.com/SCGWEDOtech/nexter-p2-nextershop-protobuf/commit/2c17262218b6ee5b64d57c3f688a5b6b99fabcc4

--- csv_grpc_pb.js
+++ csv_grpc_pb.js
@@ -7,40 +7,40 @@ var google_protobuf_descriptor_pb = require('google-protobuf/google/protobuf/des
 var product_pb = require('./product_pb.js');
 var uploaded_file_pb = require('./uploaded_file_pb.js');
 
-function serialize_csv_Request(arg) {
+function serialize_csv_v1_Request(arg) {
   if (!(arg instanceof csv_pb.Request)) {
-    throw new Error('Expected argument of type csv.Request');
+    throw new Error('Expected argument of type csv.v1.Request');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_csv_Request(buffer_arg) {
+function deserialize_csv_v1_Request(buffer_arg) {
   return csv_pb.Request.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_uploadedfile_UploadedFile(arg) {
+function serialize_uploadedfile_v1_UploadedFile(arg) {
   if (!(arg instanceof uploaded_file_pb.UploadedFile)) {
-    throw new Error('Expected argument of type uploadedfile.UploadedFile');
+    throw new Error('Expected argument of type uploadedfile.v1.UploadedFile');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_uploadedfile_UploadedFile(buffer_arg) {
+function deserialize_uploadedfile_v1_UploadedFile(buffer_arg) {
   return uploaded_file_pb.UploadedFile.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 
 var CsvServiceService = exports.CsvServiceService = {
   generate: {
-    path: '/csv.CsvService/generate',
+    path: '/csv.v1.CsvService/generate',
     requestStream: false,
     responseStream: false,
     requestType: csv_pb.Request,
     responseType: uploaded_file_pb.UploadedFile,
-    requestSerialize: serialize_csv_Request,
-    requestDeserialize: deserialize_csv_Request,
-    responseSerialize: serialize_uploadedfile_UploadedFile,
-    responseDeserialize: deserialize_uploadedfile_UploadedFile,
+    requestSerialize: serialize_csv_v1_Request,
+    requestDeserialize: deserialize_csv_v1_Request,
+    responseSerialize: serialize_uploadedfile_v1_UploadedFile,
+    responseDeserialize: deserialize_uploadedfile_v1_UploadedFile,
   },
 };
 
