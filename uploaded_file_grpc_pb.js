e7ff564b753c310e685c2022350dda4607279278
kekhuay <kekhuay@users.noreply.github.com>
2020-12-20 06:18:13 +0000

    Update from https://github.com/kekhuay/protobuf-master-example/commit/b68527f5f43821accc52e59c2545db2997c51357

--- uploaded_file_grpc_pb.js
+++ uploaded_file_grpc_pb.js
@@ -17,6 +17,17 @@ function deserialize_google_protobuf_Empty(buffer_arg) {
   return google_protobuf_empty_pb.Empty.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_uploadedfile_v1_Request(arg) {
+  if (!(arg instanceof uploaded_file_pb.Request)) {
+    throw new Error('Expected argument of type uploadedfile.v1.Request');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_uploadedfile_v1_Request(buffer_arg) {
+  return uploaded_file_pb.Request.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 function serialize_uploadedfile_v1_Response(arg) {
   if (!(arg instanceof uploaded_file_pb.Response)) {
     throw new Error('Expected argument of type uploadedfile.v1.Response');
@@ -28,10 +39,32 @@ function deserialize_uploadedfile_v1_Response(buffer_arg) {
   return uploaded_file_pb.Response.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_uploadedfile_v1_UploadedFile(arg) {
+  if (!(arg instanceof uploaded_file_pb.UploadedFile)) {
+    throw new Error('Expected argument of type uploadedfile.v1.UploadedFile');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_uploadedfile_v1_UploadedFile(buffer_arg) {
+  return uploaded_file_pb.UploadedFile.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 
 var UploadedFileServiceService = exports.UploadedFileServiceService = {
-  init: {
-    path: '/uploadedfile.v1.UploadedFileService/init',
+  create: {
+    path: '/uploadedfile.v1.UploadedFileService/create',
+    requestStream: false,
+    responseStream: false,
+    requestType: uploaded_file_pb.Request,
+    responseType: uploaded_file_pb.UploadedFile,
+    requestSerialize: serialize_uploadedfile_v1_Request,
+    requestDeserialize: deserialize_uploadedfile_v1_Request,
+    responseSerialize: serialize_uploadedfile_v1_UploadedFile,
+    responseDeserialize: deserialize_uploadedfile_v1_UploadedFile,
+  },
+  readAll: {
+    path: '/uploadedfile.v1.UploadedFileService/readAll',
     requestStream: false,
     responseStream: false,
     requestType: google_protobuf_empty_pb.Empty,
