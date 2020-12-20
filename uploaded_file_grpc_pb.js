75c52bb3bbcc8f846875da97fbb7ece61e7a32f2
kekhuay <kekhuay@users.noreply.github.com>
2020-12-20 08:36:16 +0000

    Update from https://github.com/kekhuay/protobuf-master-example/commit/1216cd0b488b26426449f85b75c64b4e999a3335

--- uploaded_file_grpc_pb.js
+++ uploaded_file_grpc_pb.js
@@ -3,29 +3,28 @@
 'use strict';
 var grpc = require('@grpc/grpc-js');
 var uploaded_file_pb = require('./uploaded_file_pb.js');
-var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js');
 var google_protobuf_timestamp_pb = require('google-protobuf/google/protobuf/timestamp_pb.js');
 
-function serialize_google_protobuf_Empty(arg) {
-  if (!(arg instanceof google_protobuf_empty_pb.Empty)) {
-    throw new Error('Expected argument of type google.protobuf.Empty');
+function serialize_uploadedfile_v1_CreateRequest(arg) {
+  if (!(arg instanceof uploaded_file_pb.CreateRequest)) {
+    throw new Error('Expected argument of type uploadedfile.v1.CreateRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_google_protobuf_Empty(buffer_arg) {
-  return google_protobuf_empty_pb.Empty.deserializeBinary(new Uint8Array(buffer_arg));
+function deserialize_uploadedfile_v1_CreateRequest(buffer_arg) {
+  return uploaded_file_pb.CreateRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_uploadedfile_v1_Request(arg) {
-  if (!(arg instanceof uploaded_file_pb.Request)) {
-    throw new Error('Expected argument of type uploadedfile.v1.Request');
+function serialize_uploadedfile_v1_QueryRequest(arg) {
+  if (!(arg instanceof uploaded_file_pb.QueryRequest)) {
+    throw new Error('Expected argument of type uploadedfile.v1.QueryRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_uploadedfile_v1_Request(buffer_arg) {
-  return uploaded_file_pb.Request.deserializeBinary(new Uint8Array(buffer_arg));
+function deserialize_uploadedfile_v1_QueryRequest(buffer_arg) {
+  return uploaded_file_pb.QueryRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 function serialize_uploadedfile_v1_Response(arg) {
@@ -56,23 +55,34 @@ var UploadedFileServiceService = exports.UploadedFileServiceService = {
     path: '/uploadedfile.v1.UploadedFileService/create',
     requestStream: false,
     responseStream: false,
-    requestType: uploaded_file_pb.Request,
+    requestType: uploaded_file_pb.CreateRequest,
     responseType: uploaded_file_pb.UploadedFile,
-    requestSerialize: serialize_uploadedfile_v1_Request,
-    requestDeserialize: deserialize_uploadedfile_v1_Request,
+    requestSerialize: serialize_uploadedfile_v1_CreateRequest,
+    requestDeserialize: deserialize_uploadedfile_v1_CreateRequest,
     responseSerialize: serialize_uploadedfile_v1_UploadedFile,
     responseDeserialize: deserialize_uploadedfile_v1_UploadedFile,
   },
-  readAll: {
-    path: '/uploadedfile.v1.UploadedFileService/readAll',
+  readBy: {
+    path: '/uploadedfile.v1.UploadedFileService/readBy',
     requestStream: false,
     responseStream: false,
-    requestType: google_protobuf_empty_pb.Empty,
+    requestType: uploaded_file_pb.QueryRequest,
     responseType: uploaded_file_pb.Response,
-    requestSerialize: serialize_google_protobuf_Empty,
-    requestDeserialize: deserialize_google_protobuf_Empty,
+    requestSerialize: serialize_uploadedfile_v1_QueryRequest,
+    requestDeserialize: deserialize_uploadedfile_v1_QueryRequest,
     responseSerialize: serialize_uploadedfile_v1_Response,
     responseDeserialize: deserialize_uploadedfile_v1_Response,
+  },
+  readOne: {
+    path: '/uploadedfile.v1.UploadedFileService/readOne',
+    requestStream: false,
+    responseStream: false,
+    requestType: uploaded_file_pb.QueryRequest,
+    responseType: uploaded_file_pb.UploadedFile,
+    requestSerialize: serialize_uploadedfile_v1_QueryRequest,
+    requestDeserialize: deserialize_uploadedfile_v1_QueryRequest,
+    responseSerialize: serialize_uploadedfile_v1_UploadedFile,
+    responseDeserialize: deserialize_uploadedfile_v1_UploadedFile,
   },
 };
 
